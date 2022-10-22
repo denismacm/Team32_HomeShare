8d8707d95d76af02f9f610932f8bc6256481cb0a
Aaron Wong <aaronwong25@hotmail.com>
2022-10-22 14:30:05 -0700

    Current Responses Feed

---
+++ app/src/main/java/com/hfad/team32_homeshare/ResponsesPage.java
@@ -0,0 +1,14 @@
+package com.hfad.team32_homeshare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResponsesPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_responses);
+    }
+
+}
