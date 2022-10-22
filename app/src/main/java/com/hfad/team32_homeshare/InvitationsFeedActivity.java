766c317c5e2c40e41faae52c36c839e68e9c3469
Michael Kim <mkim6009@usc.edu>
2022-10-22 14:35:33 -0700

    Invitations Design

---
+++ app/src/main/java/com/hfad/team32_homeshare/InvitationsFeedActivity.java
@@ -0,0 +1,15 @@
+package com.hfad.team32_homeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InvitationsFeedActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitationsfeed);
+    }
+
+}
