5a8ed1ec231372adce936782f7d3e7f0b23b9979
Aaron Wong <aaronwong25@hotmail.com>
2022-10-31 10:46:27 -0700

    Implemented Popup window/ overlay

--- app/src/main/java/com/hfad/team32_homeshare/AdapterInvitations.java
+++ app/src/main/java/com/hfad/team32_homeshare/AdapterInvitations.java
@@ -1,9 +1,12 @@
 package com.hfad.team32_homeshare;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,7 +49,29 @@ public class AdapterInvitations extends RecyclerView.Adapter<AdapterInvitations.
             @Override
             public void onClick(View view) {
                 //  IMPLEMENT OVERLAY
-                Toast.makeText(context, ""+ userEmail, Toast.LENGTH_SHORT).show(); // to show that clicking works
+//                Toast.makeText(context, ""+ userEmail, Toast.LENGTH_SHORT).show(); // to show that clicking works
+                // inflate the layout of the popup window
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.overlay_invitations, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                // dismiss the popup window when touched
+//                popupView.setOnTouchListener(new View.OnTouchListener() {
+//                    @Override
+//                    public boolean onTouch(View v, MotionEvent event) {
+//                        popupWindow.dismiss();
+//                        return true;
+//                    }
+//                });
             }
         });
 
