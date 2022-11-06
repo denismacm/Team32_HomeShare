809fa944a87767fc48d98dfca542557880ec08a3
denismacm <dmac@usc.edu>
2022-11-05 19:06:37 -0700

    Modifying Invitations Fragment

--- app/src/main/java/com/hfad/team32_homeshare/InvitationsFragment.java
+++ app/src/main/java/com/hfad/team32_homeshare/InvitationsFragment.java
@@ -3,6 +3,7 @@ package com.hfad.team32_homeshare;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,11 +11,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class InvitationsFragment extends Fragment {
     private RecyclerView recyclerView;
@@ -42,25 +51,41 @@ public class InvitationsFragment extends Fragment {
 
         adapter = new AdapterInvitations(getActivity(), invitationsList);
         recyclerView.setAdapter(adapter);
-
-        CreateDataForCards();
+        CreateDataForCards(view);
     }
 
-    private void CreateDataForCards() {
-        Invitation inv1 = new Invitation("Invitation 1", "Denis Mac");
-        invitationsList.add(inv1);
-        Invitation inv2 = new Invitation("Invitation 2", "Aaron Wong");
-        invitationsList.add(inv2);
-        Invitation inv3 = new Invitation("Invitation 3", "Michael Kim");
-        invitationsList.add(inv3);
-        Invitation inv4 = new Invitation("Invitation 4", "Lana Nguyen");
-        invitationsList.add(inv4);
-        Invitation inv5 = new Invitation("Invitation 5", "Lilly Tran");
-        invitationsList.add(inv5);
-        Invitation inv6 = new Invitation("Invitation 6", "Jefferson Nguyen");
-        invitationsList.add(inv6);
-        Invitation inv7 = new Invitation("Invitation 7", "Teresa Tran");
-        invitationsList.add(inv7);
+    private void CreateDataForCards(View view) {
+        TextView textView = view.findViewById(R.id.randomText);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("invitations").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                textView.setText("?");
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Map<String, Object> map = document.getData();
+                        Invitation inv = new Invitation(map.get("ownerID").toString(), map.get("date").toString());
+                        invitationsList.add(inv);
+                    }
+                }
+            }
+        });
+
+//        Invitation inv1 = new Invitation("Invitation 1", "Denis Mac");
+//        invitationsList.add(inv1);
+//        Invitation inv2 = new Invitation("Invitation 2", "Aaron Wong");
+//        invitationsList.add(inv2);
+//        Invitation inv3 = new Invitation("Invitation 3", "Michael Kim");
+//        invitationsList.add(inv3);
+//        Invitation inv4 = new Invitation("Invitation 4", "Lana Nguyen");
+//        invitationsList.add(inv4);
+//        Invitation inv5 = new Invitation("Invitation 5", "Lilly Tran");
+//        invitationsList.add(inv5);
+//        Invitation inv6 = new Invitation("Invitation 6", "Jefferson Nguyen");
+//        invitationsList.add(inv6);
+//        Invitation inv7 = new Invitation("Invitation 7", "Teresa Tran");
+//        invitationsList.add(inv7);
 
         adapter.notifyDataSetChanged();
     }
