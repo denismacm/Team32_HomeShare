f373000809b95bf692fffa63286b435942367062
Aaron Wong <aaronwong25@hotmail.com>
2022-11-06 20:52:55 -0800

    Implemented address API to create new invitation overlay

--- app/src/main/java/com/hfad/team32_homeshare/MainActivity.java
+++ app/src/main/java/com/hfad/team32_homeshare/MainActivity.java
@@ -1,11 +1,15 @@
 package com.hfad.team32_homeshare;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -13,20 +17,54 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 import com.hfad.team32_homeshare.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     private Spinner spin;
     private String[] homeType = new String[]{"Apartment", "Condo", "Studio", "Townhouse"};
+    private EditText editText;
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            assert data != null;
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            editText.setText(place.getAddress());
+//            textView1.setText(String.format("Locality Name: %s", place.getName()));
+//            textView2.setText(String.valueOf(place.getLatLng()));
+            float[] res = new float[3];
+            Location.distanceBetween(Objects.requireNonNull(place.getLatLng()).latitude, place.getLatLng().longitude, 34.022415, -118.285530, res);
+//            textView2.setText(String.valueOf((float) res[0]/1690) + " miles from campus");
+        } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+            assert data != null;
+            Status status = Autocomplete.getStatusFromIntent(data);
+            Toast.makeText(MainActivity.this, status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +76,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                View popupView = getLayoutInflater().inflate(R.layout.new_invitations, null);
+                @SuppressLint("InflateParams") View popupView = getLayoutInflater().inflate(R.layout.new_invitations, null);
 
                 //
-                spin = (Spinner) popupView.findViewById(R.id.homeTypeSpinner);
+                spin = popupView.findViewById(R.id.homeTypeSpinner);
                 spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
@@ -59,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spin.setAdapter(ad);
 
-                CheckBox cb = (CheckBox) popupView.findViewById(R.id.checkboxPrice);
+                CheckBox cb = popupView.findViewById(R.id.checkboxPrice);
                 cb.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -83,6 +121,17 @@ public class MainActivity extends AppCompatActivity {
                             // Price vis = 1
                             // min Price maxPrice vis = 0
                         }
+                    }
+                });
+                editText = popupView.findViewById(R.id.address);
+                Places.initialize(MainActivity.this, "AIzaSyC6VpIx6wQUPFZiA_M40pa4sBJqCzSrzfI");
+                editText.setFocusable(false);
+                editText.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME);
+                        Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList).build(MainActivity.this);
+                        startActivityForResult(intent, 100);
                     }
                 });
 
@@ -112,7 +161,6 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
-
     }
 
     private void replaceFragment(Fragment fragment) {
