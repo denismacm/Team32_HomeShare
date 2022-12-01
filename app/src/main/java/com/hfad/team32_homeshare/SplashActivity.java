1de8a5fcdd18853bffe02b8224e10b896a5b11dc
Aaron Wong <aaronwong25@hotmail.com>
2022-11-30 16:35:09 -0800

    Created Splash Screen

---
+++ app/src/main/java/com/hfad/team32_homeshare/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.hfad.team32_homeshare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
